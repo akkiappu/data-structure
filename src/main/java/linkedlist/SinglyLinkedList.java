683dac43919e62d387a1da8a5a26fa926e1130b5
INE12365985 <akhilesh.singh@tesco.com>
2019-08-28 00:07:52 +0530

    Added addNode methods

--- src/main/java/linkedlist/SinglyLinkedList.java
+++ src/main/java/linkedlist/SinglyLinkedList.java
@@ -17,16 +17,77 @@ public class SinglyLinkedList<E> {
     }
 
     public void add(E element) {
-        linkLast(element);
+        addLast(element);
     }
 
-    private void linkLast(E element) {
+    public void addFirst(E element) {
         final Node<E> newNode = new Node<>(element, null);
-        if (first == null) {
-            first = last = newNode;
-        } else {
-            last.next = newNode;
-        }
+        newNode.next = first;
+        first = newNode;
         size++;
+    }
+
+    public void add(int index, E element) {
+        checkIndexPosition(index);
+        if (index == size) {
+            addLast(element);
+        } else if(index == 0){
+            addFirst(element);
+        } else{
+            Node<E> temp = first;
+            for(int i = 1; i <index; i++){
+                temp = temp.next;
+            }
+            Node<E> newNode = new Node<>(element, null);
+            newNode.next = temp.next;
+            temp.next = newNode;
+            size++;
+        }
+    }
+
+    private void addLast(E element) {
+        if (first == null) {
+            addFirst(element);
+            last = first;
+        } else {
+            last.next = new Node<>(element, null);
+            last = last.next;
+            size++;
+        }
+    }
+
+    private void checkIndexPosition(int index) {
+        if (index < 0 || index > size)
+            throw new IndexOutOfBoundsException(outOfBoundsMsg(index));
+    }
+
+    private String outOfBoundsMsg(int index) {
+        return "Index: "+index+", Size: "+size;
+    }
+    private void print(){
+        Node<E> temp = first;
+        while (temp != null){
+            System.out.println(temp.item);
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        SinglyLinkedList<Integer> singlyLinkedList = new SinglyLinkedList<>();
+        singlyLinkedList.add(1);
+        singlyLinkedList.add(2);
+        singlyLinkedList.add(3);
+        singlyLinkedList.add(4);
+        singlyLinkedList.add(5);
+        singlyLinkedList.print();
+        singlyLinkedList.addFirst(0);
+        singlyLinkedList.print();
+        singlyLinkedList.add(1, 100);
+        singlyLinkedList.add(4, 200);
+
+       singlyLinkedList.print();
+
+
+
     }
 }
