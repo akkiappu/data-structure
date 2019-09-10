6b0ecd87c473f72bdc542c243ad702f5b77f440e
INE12365985 <akhilesh.singh@tesco.com>
2019-09-10 06:48:08 +0530

    added method to the interface and provided their impl

---
+++ src/main/java/linkedlist/List.java
@@ -0,0 +1,26 @@
+package linkedlist;
+
+public interface List<E> {
+
+    void add(E element);
+
+    void addFirst(E element);
+
+    void add(int index, E element);
+
+    int size();
+
+    void clear();
+
+    E getFirst();
+
+    E getLast();
+
+    E removeFirst();
+
+    E removeLast();
+
+    E get(int index);
+
+    E set(int index, E element);
+}
--- src/main/java/linkedlist/SinglyLinkedList.java
+++ src/main/java/linkedlist/SinglyLinkedList.java
@@ -1,6 +1,8 @@
 package linkedlist;
 
-public class SinglyLinkedList<E> {
+import java.util.NoSuchElementException;
+
+public class SinglyLinkedList<E> implements List<E> {
 
     private transient Node<E> first;
     private transient Node<E> last;
@@ -16,10 +18,12 @@ public class SinglyLinkedList<E> {
         }
     }
 
+    @Override
     public void add(E element) {
         addLast(element);
     }
 
+    @Override
     public void addFirst(E element) {
         final Node<E> newNode = new Node<>(element, null);
         newNode.next = first;
@@ -27,15 +31,16 @@ public class SinglyLinkedList<E> {
         size++;
     }
 
+    @Override
     public void add(int index, E element) {
-        checkIndexPosition(index);
+        checkIndexPosition(index, size);
         if (index == size) {
             addLast(element);
-        } else if(index == 0){
+        } else if (index == 0) {
             addFirst(element);
-        } else{
+        } else {
             Node<E> temp = first;
-            for(int i = 1; i <index; i++){
+            for (int i = 1; i < index; i++) {
                 temp = temp.next;
             }
             Node<E> newNode = new Node<>(element, null);
@@ -43,6 +48,81 @@ public class SinglyLinkedList<E> {
             temp.next = newNode;
             size++;
         }
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void clear() {
+        if(first == last){
+            first.item = null;
+            first = null;
+            last = null;
+            return;
+        }
+        Node<E> prev = first;
+        Node<E> current = first.next;
+        while(current != null){
+            first.item = null;
+            first.next = null;
+            prev = current;
+            current = current.next;
+        }
+        prev.item = null;
+        prev = null;
+        size = 0;
+    }
+
+    @Override
+    public E getFirst() {
+        if (first == null)
+            throw new NoSuchElementException();
+        return first.item;
+    }
+
+    @Override
+    public E getLast() {
+        if (last == null)
+            throw new NoSuchElementException();
+        return last.item;
+    }
+
+    @Override
+    public E removeFirst() {
+        if (first == null)
+            throw new NoSuchElementException();
+        return unlinkFirst();
+    }
+
+    @Override
+    public E removeLast() {
+        if (last == null)
+            throw new NoSuchElementException();
+        return unlinkLast();
+    }
+
+    @Override
+    public E get(int index) {
+        checkIndexPosition(index, size - 1);
+        if (index == size - 1) {
+            return last.item;
+        } else if (index == 0) {
+            return first.item;
+        } else {
+            Node<E> temp = first.next;
+            for (int i = 1; i < index; i++) {
+                temp = temp.next;
+            }
+            return temp.item;
+        }
+    }
+
+    @Override
+    public E set(int index, E element) {
+        return null;
     }
 
     private void addLast(E element) {
@@ -56,38 +136,46 @@ public class SinglyLinkedList<E> {
         }
     }
 
-    private void checkIndexPosition(int index) {
-        if (index < 0 || index > size)
+    private void checkIndexPosition(int index, int matchIndex) {
+        if (index < 0 || index > matchIndex)
             throw new IndexOutOfBoundsException(outOfBoundsMsg(index));
     }
 
     private String outOfBoundsMsg(int index) {
-        return "Index: "+index+", Size: "+size;
+        return "Index: " + index + ", Size: " + size;
     }
-    private void print(){
-        Node<E> temp = first;
-        while (temp != null){
-            System.out.println(temp.item);
-            temp = temp.next;
+
+    private E unlinkFirst() {
+        Node<E> unlinkNode = first;
+        first = first.next;
+        E e = unlinkNode.item;
+        unlinkNode.item = null;
+        if(unlinkNode == last){
+            last = null;
         }
+        unlinkNode = null;
+        size--;
+        return e;
     }
 
-    public static void main(String[] args) {
-        SinglyLinkedList<Integer> singlyLinkedList = new SinglyLinkedList<>();
-        singlyLinkedList.add(1);
-        singlyLinkedList.add(2);
-        singlyLinkedList.add(3);
-        singlyLinkedList.add(4);
-        singlyLinkedList.add(5);
-        singlyLinkedList.print();
-        singlyLinkedList.addFirst(0);
-        singlyLinkedList.print();
-        singlyLinkedList.add(1, 100);
-        singlyLinkedList.add(4, 200);
+    private E unlinkLast() {
+        if(first == last){
+            return unlinkFirst();
+        }
+        Node<E> prev = first;
+        Node<E> current = first.next;
 
-       singlyLinkedList.print();
+        while(current.next != null){
+            prev = current;
+            current = current.next;
+        }
+        prev.next = null;
+        last = prev;
+        E e = current.item;
+        current.item = null;
+        current = null;
 
-
-
+        return e;
     }
+
 }
