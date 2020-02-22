0353f5e0a827c861bcbc47b48f86564087f6bfb0
Akhilesh Singh <akhilesh.s.singh@oracle.com>
2020-02-22 22:14:36 +0530

    Update recursive logic

--- src/main/java/linkedlist/problems/ReverseSinglyLinkListRecursive.java
+++ src/main/java/linkedlist/problems/ReverseSinglyLinkListRecursive.java
@@ -3,17 +3,13 @@ package linkedlist.problems;
 import linkedlist.impl.SinglyLinkedList;
 
 public class ReverseSinglyLinkListRecursive<E> {
-    private Node<E> head;
-    public ReverseSinglyLinkListRecursive(Node<E> head){
-        this.head = head;
-    }
     public Node<E> reverseRecursive(Node<E> node) {
-        if (node.next == null) {
-            return head = node;
+        if (node == null || node.next == null) {
+            return node;
         }
         Node<E> result = reverseRecursive(node.next);
-        result.next = node;
+        node.next.next = node;
         node.next = null;
-        return node;
+        return result;
     }
 }
